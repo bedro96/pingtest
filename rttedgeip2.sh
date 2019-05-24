6cc429e557cb16fe7386094328d8420ddbd258b3
Kunho Ko <bedro96@naver.com>
2019-05-24 23:22:34 +0900

    Update rttedgeip2 and 3

---
+++ rttedgeip2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#systemctl stop firewalld
+rm edgeiplist*
+rm output.txt
+wget https://raw.githubusercontent.com/bedro96/pingtest/master/edgeiplist2.txt
+input="./edgeiplist2.txt"
+while IFS= read -r var
+do
+        pingresult=$(ping $var -i 1 -c 3 | tail -1)
+        echo "ping $var: $pingresult"
+        min=$(echo $pingresult | awk -F / '{print $4}' | cut -b 8- | sed 's/....$//' )
+        avg=$(echo $pingresult | awk -F / '{print $5}' | sed 's/....$//' )
+        max=$(echo $pingresult | awk -F / '{print $6}' | sed 's/....$//' )
+        echo "min: $min"
+        echo "avg: $avg"
+        echo "max: $max"
+        echo "$var,$min,$avg,$max" >> output.txt
+done < "$input"
